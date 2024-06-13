c93a139efd151cf64e775e0a11f4c6e0d1b7ad68
Philipp1343 <phabti@gmail.com>
2024-06-13 23:40:54 +0200

    triangular

---
+++ Sample_test/triangular_spmv/step_02_launch_job.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --time=00:15:00
+#SBATCH -N 1
+#SBATCH -e error.txt
+#SBATCH -o output.txt
+#SBATCH --ntasks-per-node=24
+
+module load julia/1.10.3
+module load intel/mkl/64/11.2/2015.5.223
+module load openmpi/gcc/64/4.0.2
+module load petsc/3.16.1
+
+# Disable multi-threading
+export OMP_NUM_THREADS=1
+export NUM_THREADS=1
+
+# This file is to be launched with sbatch:
+# $ sbatch step_02_launch_job.sh
+
+# Use the switches above to select the number of nodes
+# and the number of tasks (processes) per node
+
+# Use different values of parts_per_dir, nodes_per_dir
+
+# NB! the value passed to np needs to coincide with prod(parts_per_dir)
+MPIFLAGS="--map-by node:span --rank-by core"
+JULIAFLAGS="--project=. --check-bounds=no -O3"
+mpiexec -np 1 $MPIFLAGS  julia $JULIAFLAGS -e '
+    include("experiment.jl")
+    with_mpi() do distribute
+        params = Dict(
+            "parts_per_dir"=>(1,1,1),
+            "nodes_per_dir"=>(100,100,100),
+           )
+        nruns = 4
+        main(distribute,params,nruns)
+    end
+'
