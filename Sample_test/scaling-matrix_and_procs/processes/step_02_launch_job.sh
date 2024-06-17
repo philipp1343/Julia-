0349996405f14b5cca959a91fcaa39ae09c5f593
Philipp1343 <phabti@gmail.com>
2024-06-17 14:23:39 +0200

    100000000

--- Sample_test/scaling-matrix_and_procs/processes/step_02_launch_job.sh
+++ Sample_test/scaling-matrix_and_procs/processes/step_02_launch_job.sh
@@ -30,7 +30,7 @@ mpiexec -np 32 $MPIFLAGS  julia $JULIAFLAGS -e '
     with_mpi() do distribute
         params = Dict(
             "parts_per_dir"=>(4,4,2),
-            "nodes_per_dir"=>(3000,100,100),
+            "nodes_per_dir"=>(1000,1000,100),
            )
         nruns = 4
         main(distribute,params,nruns)
