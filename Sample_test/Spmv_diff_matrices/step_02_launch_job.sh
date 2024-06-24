7c00e343075e5ab6ba44ace95539f4929bcad3c4
Philipp1343 <phabti@gmail.com>
2024-06-24 18:41:03 +0200

    128

--- Sample_test/Spmv_diff_matrices/step_02_launch_job.sh
+++ Sample_test/Spmv_diff_matrices/step_02_launch_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=00:15:00
-#SBATCH -N 2
+#SBATCH -N 4
 #SBATCH -e error.txt
 #SBATCH -o output.txt
 #SBATCH --ntasks-per-node=32
@@ -25,11 +25,11 @@ export NUM_THREADS=1
 # NB! the value passed to np needs to coincide with prod(parts_per_dir)
 MPIFLAGS="--map-by node:span --rank-by core"
 JULIAFLAGS="--project=. --check-bounds=no -O3"
-mpiexec -np 64 $MPIFLAGS  julia $JULIAFLAGS -e '
+mpiexec -np 128 $MPIFLAGS  julia $JULIAFLAGS -e '
     include("experiment.jl")
     with_mpi() do distribute
         params = Dict(
-            "parts_per_dir"=>(4,4,4),
+            "parts_per_dir"=>(8,4,4),
             "nodes_per_dir"=>(10,100,100),
            )
         nruns = 25
